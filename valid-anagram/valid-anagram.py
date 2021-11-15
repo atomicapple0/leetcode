1ece61b0b5bcc82c991cc2f056b7108dae94c571
Brian Zhang <30697412+atomicapple0@users.noreply.github.com>
2021-11-14 22:20:19 -0500

    Time: 36 ms (95.04%), Space: 14.6 MB (56.84%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,24 +1,5 @@
-# s,t
-# dicts = {'a':#a, 'b':#b, ...}
-# dictt = {'a':#a, 'b':#b, ...}
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict_s = defaultdict(int)
-        dict_t = defaultdict(int)
-        
-        for c in s:
-            dict_s[c] += 1
-        
-        for c in t:
-            dict_t[c] += 1
-            
-        for k in dict_s.keys():
-            if dict_s[k] != dict_t[k]:
-                return False
-        
-        for k in dict_t.keys():
-            if dict_s[k] != dict_t[k]:
-                return False
-        
-        return True
+        return Counter(s) == Counter(t)
         
